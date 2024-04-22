be9ed8b85690f6082f7b7e3e0fd8fa88b63565e2
Jawad Rashid <jawadrashid2010@gmail.com>
2024-04-22 20:51:38 +0500

    Update streamlit_app.py
    Added requests code for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,11 @@ if ingredients_list:
     # st.stop()
             
     time_to_insert = st.button('Submit Order')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
